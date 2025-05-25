e7d5cdf0b3b2cd62b2fa7f4012ca62e6c0c80b42
Maxim <maxim.bushnin@gmail.com>
2025-05-25 20:51:21 +0300

    Исправлен способ получения BOT_TOKEN из переменных окружения

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()  # загружает переменные из .env файла (если запускаешь локально)
 
-BOT_TOKEN = os.getenv("8135011284:AAFfweFaH5SLJBbHBbjF8c820ck5cS0JRiA")
-SHOP_ID = os.getenv("1089533")
-SECRET_KEY = os.getenv("live_UPCft_ahWDxKs-4mNrJZ4OWoqAcYgOCdPwdtJu0RMO0")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SHOP_ID = os.getenv("SHOP_ID")
+SECRET_KEY = os.getenv("SECRET_KEY")
