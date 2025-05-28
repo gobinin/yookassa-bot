1d3a9b8594876cb99558bae2073b9082647dfe76
Maxim <maxim.bushnin@gmail.com>
2025-05-29 00:22:58 +0300

    Автообновление: 2025-05-29 00:22:58

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные окружения из .env-файла
 load_dotenv()
 
-# Получаем значения переменных
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-SHOP_ID = str(os.getenv("SHOP_ID"))
+SHOP_ID = os.getenv("SHOP_ID")
 SECRET_KEY = os.getenv("SECRET_KEY")
